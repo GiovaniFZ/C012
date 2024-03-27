04df8f1e62a5df454f5c9a0d901630a865445fee
Giv <giovanifinazzi@gmail.com>
2024-03-27 19:37:22 -0300

    Update

--- ProjetoCars/src/CarThread.java
+++ ProjetoCars/src/CarThread.java
@@ -1,21 +1,29 @@
-import javax.security.auth.callback.TextInputCallback;
 import java.util.Random;
 
-public class CarThread extends Thread{
-    boolean isRunning = true;
+public class CarThread extends Thread {
+    private int carroId;
+
+    @Override
     public void run() {
-            int distance = 0;
-            while (distance <= 100) {
-                Random random = new Random();
-                int velocidade = random.nextInt(20);
-                distance = distance + velocidade;
-                System.out.println(Thread.currentThread().getName() + " andou " + velocidade + "m está à "  + (100 - distance) + "m do final!");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    break;
-                }
+        int distance = 0;
+        while (distance < 100 && !isInterrupted()) {
+            Random random = new Random();
+            int velocidade = random.nextInt(21);
+            distance += velocidade;
+            System.out.println("Carro " + carroId + " com velocidade " + velocidade + "m/s está a " + (100 - distance) + "m do final!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Carro " + carroId + " interrompido.");
+                return;
             }
-            isRunning = false;
+        }
+        if (distance >= 100) {
+            Main.setVencedor(carroId);
+        }
+    }
+
+    public void setId(int carroId) {
+        this.carroId = carroId;
     }
 }
--- ProjetoCars/src/Main.java
+++ ProjetoCars/src/Main.java
@@ -1,12 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        CarThreadAux carThreadAux = new CarThreadAux();
-        carThreadAux.instanciarCarros();
-        carThreadAux.start();
-        //System.out.println("Numero de carros: ");
-        //Scanner sc = new Scanner(System.in);
-        //nCarros = sc.nextInt();
+    private static int nCarros;
+    private static int carroVencedor;
+    private static boolean corridaFinalizada = false;
+
+    public static void main(String[] args) {
+        System.out.println("Numero de carros: ");
+        Scanner sc = new Scanner(System.in);
+        nCarros = sc.nextInt();
+
+        for (int i = 1; i <= nCarros; i++) {
+            CarThread carThread = new CarThread();
+            carThread.setId(i);
+            carThread.start();
+        }
+    }
+
+    public static synchronized void setVencedor(int carroId) {
+        if (!corridaFinalizada) {
+            carroVencedor = carroId;
+            corridaFinalizada = true;
+            System.out.println("Carro " + carroVencedor + " venceu!");
+            for (Thread t : Thread.getAllStackTraces().keySet()) {
+                if (t instanceof CarThread && t != Thread.currentThread()) {
+                    ((CarThread) t).interrupt();
+                }
+            }
+        }
     }
 }
